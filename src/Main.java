50a1ab7caad3da114ec42634b17333cdabf5002f
Jonathan Tromp <jonatromp45@gmail.com>
2025-02-07 17:37:01 +0000

    More attempts at making the snap game work, successfully added user interaction functionality. Requires ability to compare two card suit values to provide a winner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        CardGame c = new CardGame();
-        c.shuffleDeck();
-        c.dealCard();
+//        CardGame c = new CardGame();
+//        c.shuffleDeck();
+//        c.dealCard();
+//        c.getDeck();
+
+        Snap snap = new Snap();
+
+
+
     }
 }
+
--- src/Snap.java
+++ src/Snap.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class Snap extends CardGame {
 
+    Card[] deck = new Card[52];
+
+    public Snap() {
+    System.out.println(deck);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Snap! Press Enter to Begin...");
+        scanner.nextLine();
+        shuffleDeck();
+        dealCard();
+        System.out.println("Press Enter For Next Card...");
+        scanner.nextLine();
+//        for (int i = 1; i < deckOfCards.size(); i++) {
+//            System.out.println("Next card is : " + deckOfCards.get(i));
+//            if (deckOfCards.get(i).getSuit() == )
+//        }
+
+
+
+    }
 }
