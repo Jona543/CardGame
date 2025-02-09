4229fe06e364a673c79999b74e1d8d65acb2cb09
Jonathan Tromp <jonatromp45@gmail.com>
2025-02-09 22:49:23 +0000

    Added functionality to make winner type snap before they win, errors marked in notes

--- src/Snap.java
+++ src/Snap.java
@@ -9,10 +9,11 @@ public class Snap extends CardGame {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Snap! Press Enter to Begin...");
         scanner.nextLine();
-        System.out.println("If you would like to play with 2 players, please type 2 then press enter to begin, otherwise just press enter ");
+        String newLine = System.lineSeparator();
+        System.out.println("If you would like to play with 2 players," + newLine + "please type 2 then press enter to begin," + newLine + "otherwise type 1 then press enter");
         int players = scanner.nextInt();
 
-        if (players == 2){
+        if (players == 2) {
             System.out.print("Welcome to two player snap! Player one please enter your name: ");
             String playerOneName = scanner.next();
             System.out.print("Player two please enter your name: ");
@@ -25,27 +26,43 @@ public class Snap extends CardGame {
             scanner.nextLine();
 
             shuffleDeck();
-            for (int i = 0; i < 52; i++){
+            for (int i = 0; i < 52; i++) {
                 deck[i] = deckOfCards.get(i);
             }
-
             System.out.println(playerOne.getName() + ", your first card is " + deck[0]);
             scanner.nextLine();
             System.out.println(playerTwo.getName() + ", your first card is " + deck[1]);
-            if (deck[0].getSymbol().equals(deck[1].getSymbol())){
-                System.out.println(playerTwo.getName() + " wins! Your card is the " + deck[1] + " Game Over");
-                scanner.close();
+            if (deck[0].getSymbol().equals(deck[1].getSymbol())) {
+                System.out.println(playerTwo.getName() + ", your card is the " + deck[1] + " Type snap to win!");
+                String snap = scanner.nextLine();
+//                scanner.wait(2000); how to allow the user 2 seconds to type in snap before the else statement occurs and the other player wins
+                if (snap.equals("snap")){
+                    System.out.println(playerTwo.getName() + " wins! Game Over");
+                    scanner.nextLine();
+                    scanner.close();
+                    // error after game finishes 'exception in thread "main"'
+                } else {
+                    System.out.println("Too slow, " + playerOne.getName() + " wins!");
+                    scanner.close();
+                }
             }
             scanner.nextLine();
 
-            for (int i = 2; i < deck.length; i++){
+            for (int i = 2; i < deck.length; i++) {
                 boolean winner = false;
-                if (winner){
+                if (winner) {
                     scanner.close();
                 }
-                if (i % 2 == 0){
-                    if (deck[i - 1].getSymbol().equals(deck[i].getSymbol())) {
-                        System.out.println(playerOne.getName() + " wins! Your card is the " + deck[i] + " Game Over");
+                if (i % 2 == 0) {
+                    if (deck[i - 1].getSymbol().equals(deck[i].getSymbol())) {System.out.println(playerOne.getName() + ", your card is the " + deck[i] + " Type snap to win!");
+                        String snap = scanner.nextLine();
+//                scanner.wait(2000); how to allow the user 2 seconds to type in snap before the else statement occurs and the other player wins
+                        if (snap.equals("snap")){
+                            System.out.println(playerOne.getName() + " wins! Game Over");
+                        } else {
+                            System.out.println("Too slow, " + playerTwo.getName() + " wins!");
+                        }
+
                         winner = true;
                         break;
                     } else {
@@ -54,7 +71,14 @@ public class Snap extends CardGame {
                     }
                 } else {
                     if (deck[i - 1].getSymbol().equals(deck[i].getSymbol())) {
-                        System.out.println(playerTwo.getName() + " wins! Your card is the " + deck[i] + " Game Over");
+                        System.out.println(playerTwo.getName() + ", your card is the " + deck[i] + " Type snap to win!");
+                        String snap = scanner.nextLine();
+//                scanner.wait(2000); how to allow the user 2 seconds to type in snap before the else statement occurs and the other player wins
+                        if (snap.equals("snap")){
+                            System.out.println(playerTwo.getName() + " wins! Game Over");
+                        } else {
+                            System.out.println("Too slow, " + playerOne.getName() + " wins!");
+                        }
                         winner = true;
                         break;
                     } else {
@@ -64,30 +88,30 @@ public class Snap extends CardGame {
 
                 }
             }
+        } else if (players == 1) {
 
-        }
+            scanner.nextLine();
 
-        scanner.nextLine();
+            shuffleDeck();
+            for (int i = 0; i < 52; i++) {
+                deck[i] = deckOfCards.get(i);
+            }
+            System.out.println("Your card is the " + deck[0]);
+            System.out.println("Press Enter For Next Card...");
+            scanner.nextLine();
 
-        shuffleDeck();
-        for (int i = 0; i < 52; i++){
-            deck[i] = deckOfCards.get(i);
-        }
-        System.out.println("Your card is the " + deck[0]);
-        System.out.println("Press Enter For Next Card...");
-        scanner.nextLine();
-
-        for (int i = 1; i < deck.length; i++) {
-            if (deck[i - 1].getSymbol().equals(deck[i].getSymbol())) {
-                System.out.println("Snap! Your card is the " + deck[i] + " Game Over");
-                scanner.close();
-                break;
-            } else {
-                System.out.println("Your next card is the " + deck[i]);
-                scanner.nextLine();
+            for (int i = 1; i < deck.length; i++) {
+                if (deck[i - 1].getSymbol().equals(deck[i].getSymbol())) {
+                    System.out.println("Snap! Your card is the " + deck[i] + " Game Over");
+                    scanner.close();
+                    break;
+                } else {
+                    System.out.println("Your next card is the " + deck[i]);
+                    scanner.nextLine();
+                }
             }
         }
+
+        // is there a way to make the thread go back to the top of the game if it gets through the whole deck and no pairs have occurred? I.e. shuffle the deck again then go through the whole process again
     }
-
-
 }
